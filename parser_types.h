8b2d1011a67c3cf96d3a638c9b01476931e42c07
CEL2024 <136928568+CEL2024@users.noreply.github.com>
2023-11-04 21:12:39 -0400

    Add files via upload

---
+++ parser_types.h
@@ -0,0 +1,12 @@
+/* $Id: parser_types.h,v 1.1 2023/10/04 03:43:15 leavens Exp $ */
+#ifndef _PARSER_TYPES_H
+#define _PARSER_TYPES_H
+
+#include "ast.h"
+
+// The type of Bison's parser stack elements (parse values)
+// alternative for older C compilers #define YYSTYPE AST
+typedef AST YYSTYPE;
+#define YYSTYPE_IS_DECLARED 1
+
+#endif
